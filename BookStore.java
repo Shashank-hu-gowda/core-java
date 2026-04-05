4022b132a08de4306d9cbe38534525403133fc6b
Shashank-hu-gowda <shashankhushashu@gmail.com>
2026-04-05 11:07:19 +0530

    class

---
+++ BookStore.java
@@ -0,0 +1,18 @@
+Class BookStore {
+
+    static void store() {
+        System.out.println("Welcome to Book Store");
+    }
+
+    static void reader(String name) {
+        System.out.println("Customer buying book: " + name);
+    }
+
+    static int bookPrice() {
+        return 420;
+    }
+
+    static int total(int price) {
+        return price + 30;
+    }
+ }
